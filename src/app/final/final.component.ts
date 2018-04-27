bc6e23cab916cbdaf9f78b0b2707a90696791a12
李家伟 <1341924634@qq.com>
2018-04-27 18:15:43 +0800

    20180427 李家伟 act-test-send

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { HttpParams } from "@angular/common/http";
+
 @Component({
   selector: 'app-final',
   templateUrl: './final.component.html',
@@ -9,6 +12,7 @@ export class FinalComponent implements OnInit {
 
   thankWord = "Thank u for reading, and now please reply.";
   private msg = "";
+  sendUrl = "assets/sendmsg.json";
   sendMsg = function () {
     console.log(this.msg);
   }
@@ -19,12 +23,41 @@ export class FinalComponent implements OnInit {
       this.thankWord = "Thank u for reading, and now please reply.";
     }
   }
-  
-  constructor() {
+  /* replyMsg = function() {
+    console.log("replying");
+  } */
+  constructor(private http: HttpClient) {
     this.sendMsg = function () {
+      //1.捕获打印回复的信息，页面相应
       console.log("idea:" + this.msg);
       if ("" !== this.msg) {
-        this.thankWord = "Thank u for replying.";
+        //var that = this;
+        this.thankWord = "Thank u for replying...";
+        //2.使用webwork开启邮件回复发送线程
+        //this.thankWord = "Thank u for replying.";
+        //var worker = new Worker("messagesend.js");
+        //worker.onmessage = function (event) {
+          //document.getElementById('result').innerHTML += event.data + "<br/>";
+          //that.thankWord = "Reply success.";
+        //};
+        //const headers = new HttpHeaders().set("Content-Type", "application/json");
+        //2.使用post写入本地json文件-404
+        this.http
+          .put(this.sendUrl, {
+            "replyMsg": this.msg
+          })
+          //.subscribe(val => console.log('Value emitted successfully', val));
+          .subscribe(
+            //val => this.msgs = val,
+            error => {
+              console.error("Http failed: ", error);
+              this.thankWord = "Reply failed.";
+            },
+            () => {
+              console.log("HTTP completed.");
+              this.thankWord = "Reply success.";
+            }
+          );
       }
     } 
    }
